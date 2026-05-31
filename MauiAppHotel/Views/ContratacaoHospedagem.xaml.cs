003828c762680a945ea7a31f6b563ad197b005bc
lsqn0 <lydiasquino@gmail.com>
2026-05-31 19:05:32 -0300

    Adições.

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -5,6 +5,7 @@ public partial class ContratacaoHospedagem : ContentPage
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
+                dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
 	}
 
     private async void Sobre_Clicked(object sender, EventArgs e)
